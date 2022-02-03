a5c0f99f883a4d568f2ab2409217c2485b04d38d
fredo <fmaurtino@gmail.com>
2022-02-03 10:55:33 +0700

    wohhale

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,104 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <title>WOHHALE</title>
+        <meta name="description" content="">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <link rel="stylesheet" href="style.css">
+        <!-- <link rel="stylesheet" href="fontawesome/all.css"> -->
+        <script src="https://kit.fontawesome.com/39ee41da90.js" crossorigin="anonymous"></script>
+        <!-- <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous"> -->
+    </head>
+    <body>
+        <header>Jangan cuma sayang ke dia aja, tapi juga sayangi dirimu dengan minum "WOHHALE"</header>
+        <nav>
+            <div class="navbar-brand" href="/">WOHHALE.</div>
+            <ul>
+                <li><a href="/" class="active">Home</a></li>
+                <li><a href="#Products">Product</a></li>
+                <li><a href="#about_us">About us</a></li>
+                <li><a href="#contact">Contact</a></li>
+            </ul>
+        </nav>
+        <div class="banner">
+            <h1>Selamat Datang di WOHHALE.</h1>
+            <h2>Minuman jus sehat<h2>
+        </div>
+        <section id="Products" class="section">
+            <h1>PRODUCTS</h1>
+            <div class="container-product">
+                <div class="card">
+                    <img src="img/jus_card.png" width="250">
+                    <div class="card-product">
+                        <h4>Jus Jambu</h4>
+                        <p>1. Menjaga kesehatan jantung <br> 2. Mencegah penuaan dini <br> 3. Meningkatkan daya tahan tubuh <br> 4. Kaya akan kalium, antioksidan, antosionin, vitamin C</p>
+                    </div>
+                </div>
+                <div class="card">
+                    <img src="img/jus_card.png" width="250">
+                    <div class="card-product">
+                        <h4>Jus Jambu</h4>
+                        <p>1. Menjaga kesehatan jantung <br> 2. Mencegah penuaan dini <br> 3. Meningkatkan daya tahan tubuh <br> 4. Kaya akan kalium, antioksidan, antosionin, vitamin C</p>
+                    </div>
+                </div>
+                <div class="card">
+                    <img src="img/jus_card.png" width="250">
+                    <div class="card-product">
+                        <h4>Jus Jambu</h4>
+                        <p>1. Menjaga kesehatan jantung <br> 2. Mencegah penuaan dini <br> 3. Meningkatkan daya tahan tubuh <br> 4. Kaya akan kalium, antioksidan, antosionin, vitamin C</p>
+                    </div>
+                </div>
+                <div class="card">
+                    <img src="img/jus_card.png" width="250">
+                    <div class="card-product">
+                        <h4>Jus Jambu</h4>
+                        <p>1. Menjaga kesehatan jantung <br> 2. Mencegah penuaan dini <br> 3. Meningkatkan daya tahan tubuh <br> 4. Kaya akan kalium, antioksidan, antosionin, vitamin C</p>
+                    </div>
+                </div>
+                <div class="card">
+                    <img src="img/jus_card.png" width="250">
+                    <div class="card-product">
+                        <h4>Jus Jambu</h4>
+                        <p>1. Menjaga kesehatan jantung <br> 2. Mencegah penuaan dini <br> 3. Meningkatkan daya tahan tubuh <br> 4. Kaya akan kalium, antioksidan, antosionin, vitamin C</p>
+                    </div>
+                </div>
+            </div>
+        </section>
+        <section id="about_us" class="section">
+            <h1>ABOUT US</h1>
+                <div class="about-us">
+                    <img src="img/jus_alpukat.jpg" width="300" alt="Foto product">
+                    <div class="keterangan">
+                        <h3>WOHHALE</h3>
+                        <p>Wohhale adalah usaha kecil yang menghasilkan sebuah jus dengan bahan-bahan yang tidak menggunakan pengawet sehingga membuat tubuh kita sehat.</p>
+                    </div>
+                </div>
+        </section>
+        <footer id="contact">
+            <h1>CONTACT</h1>
+            <div class="footer">
+                <div class="footer-items">
+                    <h4>Social Media</h4>
+                    <span><i class="fab fa-instagram-square fa-lg"> Instagram</i></span>
+                    <i class="fab fa-facebook-square fa-lg"> Facebook</i>
+                </div>
+                <div class="footer-items">
+                    <h4>Information</h4>
+                    <i class="fab fa-whatsapp-square fa-lg"> 082137054816</i>
+                    <i class="fab fa-line fa-lg"> fredomaurtino</i>
+                </div>
+                <div class="footer-items">
+                    <h4>Founder</h4>
+                    <h5>Sonia</h5>
+                    <h5>Nanda</h5>
+                </div>
+            </div>
+            <div class="copyright">
+                <h4>Dibuat dengan sepenuh hati</h4>
+                <h5>© Fredo Maurtino.</h5>
+            </div>
+        </footer>
+    </body>
+</html>
